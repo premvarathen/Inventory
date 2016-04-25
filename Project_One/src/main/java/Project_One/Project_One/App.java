4b3a25cb09548db22160a0788597d7d7858efa52
Premvarathen Ramasundaram <premvarathen@Premvarathens-MacBook-Pro.local>
2016-04-24 20:59:22 -0500

    first commit

---
+++ Project_One/src/main/java/Project_One/Project_One/App.java
@@ -0,0 +1,13 @@
+package Project_One.Project_One;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
